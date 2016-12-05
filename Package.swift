4ad0984d272e7eef9934cb23aa689bcd7d60b6e3
Matthias Ludwig <mludwig@hausgross.de>
2016-12-05 22:30:48 +0100

    - update to vapor 1.2

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "CocoaheadsSaar",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 2),
         .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 1)
     ],
     exclude: [
